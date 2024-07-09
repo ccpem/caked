41d40b3dfe9a2216c68c54509c8daeb549b0fad3
Luc Elliott <hllelli2@liverpool.ac.uk>
2024-07-09 16:50:12 +0000

    first attempt at reducing if label/weight... is not none repeated code

---
+++ src/caked/Wrappers/__init__.py
@@ -0,0 +1,11 @@
+from functools import wraps
+
+
+def none_return_none(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if args[0] is None:
+            return None
+        return func(*args, **kwargs)
+
+    return wrapper
