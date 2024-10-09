7fbefdfe93544f706350e00da509b8885264aba2
hllelli2 <hllelli2@liverpool.ac.uk>
2024-10-09 13:54:43 +0000

    Refactor test fixtures to use default scope and improve readability

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,25 +7,25 @@ from tempfile import TemporaryDirectory
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_data_mrc_dir():
     """Fixture to provide the MRC test data directory."""
     return Path(Path(__file__).parent.joinpath("testdata_mrc"))
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_data_npy_dir():
     """Fixture to provide the NPY test data directory."""
     return Path(Path(__file__).parent.joinpath("testdata_npy"))
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_corrupt_file():
     """Fixture to provide the path to a corrupt file for testing."""
     return Path(__file__).parent / "corrupt.mrc"
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def test_data_single_mrc_dir():
     """Fixture to provide a single MRC file for testing."""
     return Path(Path(__file__).parent.joinpath("testdata_mrc", "mrc"))
